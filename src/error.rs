6f140b581eb440ad9f56000c4405363b42a5338e
Christian Ehrig <christian@ehrig.io>
2026-05-18 00:42:31 +0200

    implement error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,7 @@
+use std::ffi::NulError;
+use std::fmt::{Display, Formatter};
+use std::num::ParseFloatError;
+
 #[derive(Debug)]
 pub enum Error {
     UnknownEvent,
@@ -9,14 +13,37 @@ pub enum Error {
     D3ContextMissing,
     D3RenderTargetMissing,
     LockFailed,
+    VolumeNotInRange,
     Generic(Box<dyn std::error::Error + Send + Sync>),
+    Nul(NulError),
+    ParseFloat(ParseFloatError),
+    Io(std::io::Error),
+    #[cfg(target_os = "windows")]
+    Windows(windows_core::Error),
 }
 
-impl<E> From<E> for Error
-where
-    E: Into<Box<dyn std::error::Error + Send + Sync>>,
-{
-    fn from(value: E) -> Self {
-        Error::Generic(value.into())
+impl From<NulError> for Error {
+    fn from(value: NulError) -> Self {
+        Error::Nul(value)
     }
 }
+
+impl From<ParseFloatError> for Error {
+    fn from(value: ParseFloatError) -> Self {
+        Error::ParseFloat(value)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Error::Io(value)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -3,7 +3,14 @@ pub mod events;
 pub mod utility;
 pub mod window;
 
+use crate::error::Error;
 pub use elements::*;
 pub use events::*;
 pub use utility::*;
 pub use window::*;
+
+impl From<windows_core::Error> for Error {
+    fn from(value: windows_core::Error) -> Self {
+        Error::Windows(value)
+    }
+}
